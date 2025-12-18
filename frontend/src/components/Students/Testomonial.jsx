42dc5f3ba77fd72e4f1918b186c61b8d04529524
biswasuser <lohanibiswas1@gmail.com>
2025-12-18 20:02:35 +0545

    completed testomonial section

--- frontend/src/components/Students/Testomonial.jsx
+++ frontend/src/components/Students/Testomonial.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { assets, dummyTestimonial } from "../../assets/assets";
 
 const Testomonial = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+   <div className="flex flex-col items-center justify-center p-6 md:p-14 w-full bg-gradient-to-r from-blue-200 to-blue-500  text-black">
+            <a className="mb-8 md:mb-12" href="https://prebuiltui.com">
+                <img className="h-10" src={assets.logo} alt="logo white" />
+            </a>
+            <div className="flex flex-col items-center">
+                <p className="md:text-3xl text-xl text-center">Our learners are at the heart of everything we do. Explore their inspiring stories of growth, success, and how we helped them achieve their goals.</p>
+                <div className="flex items-center gap-2 mt-8">
+                    <img className="w-12 h-12 md:w-16 md:h-16 rounded-full" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=100" alt="profileImg1" />
+                    <div className="text-sm">
+                        <p className="font-medium text-lg">Donald Jackman</p>
+                        <p>SWE 1 @ Amazon</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex items-center gap-2 mt-8">
+                <div className="w-3 h-3 md:w-4 md:h-4 bg-white rounded-full"></div>
+                <div className="w-3 h-3 md:w-4 md:h-4 bg-slate-400 rounded-full"></div>
+                <div className="w-3 h-3 md:w-4 md:h-4 bg-slate-400 rounded-full"></div>
+            </div>
+        </div>
+  );
+};
 
-export default Testomonial
+export default Testomonial;
--- frontend/src/pages/Student/Home.jsx
+++ frontend/src/pages/Student/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../../components/Students/Hero'
 import CoursesSection from '../../components/Students/CoursesSection'
+import Testomonial from '../../components/Students/Testomonial'
 
 
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero/>
       <CoursesSection/>
+      <Testomonial/>
       
     </div>
   )
